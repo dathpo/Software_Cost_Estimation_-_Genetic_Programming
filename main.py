bed024c5e340a0ee32246eed44f42648c2572f3c
David <davidtpocock@gmail.com>
2019-01-16 14:53:54 +0000

    Finalise project, push last changes

--- main.py
+++ main.py
@@ -4,17 +4,16 @@ __author__ = 'David T. Pocock'
 import random
 from pathlib import Path
 from parser import Parser
-import seaborn as sns; sns.set()
 import os.path
 import operator
+import numpy as np
+import pandas as pd
+from deap import algorithms, base, creator, tools, gp
 from sklearn.model_selection import KFold, train_test_split
 from sklearn.linear_model import LinearRegression
-import numpy as np
-from deap import algorithms, base, creator, tools, gp
-import seaborn as sns; sns.set()
 from sklearn.preprocessing import StandardScaler
+import seaborn as sns; sns.set()
 import matplotlib.pyplot as plt
-import pandas as pd
 
 pd.options.mode.chained_assignment = None
 
@@ -30,70 +29,8 @@ def main():
 
     exec_k_fold(df, pset, True)
     exec_k_fold(df, pset, False)
+    # exec_train_test_split(df, pset, True)
     # exec_train_test_split(df, pset, False)
-
-
-def execute_ea(pset, train_data, test_data):
-    global external_call
-    pop_num = 300
-    external_call = False
-    creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
-    creator.create("Individual", gp.PrimitiveTree, fitness=creator.FitnessMin)
-
-    toolbox = base.Toolbox()
-    toolbox.register("expr", gp.genHalfAndHalf, pset=pset, min_=1, max_=2)
-    toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.expr)
-    toolbox.register("population", tools.initRepeat, list, toolbox.individual)
-    toolbox.register("compile", gp.compile, pset=pset)
-    fitness_function = get_fitness(toolbox, 3)
-    toolbox.register("evaluate", fitness_function, train_data)
-    toolbox.register("select", tools.selTournament, tournsize=3)
-    toolbox.register("mate", gp.cxOnePoint)
-    toolbox.register("expr_mut", gp.genFull, min_=0, max_=2)
-    toolbox.register("mutate", gp.mutUniform, expr=toolbox.expr_mut, pset=pset)
-
-    toolbox.decorate("mate", gp.staticLimit(key=operator.attrgetter("height"), max_value=17))
-    toolbox.decorate("mutate", gp.staticLimit(key=operator.attrgetter("height"), max_value=17))
-
-    random.seed(1000)
-    pop = toolbox.population(n=pop_num)
-    hof = tools.HallOfFame(1)
-
-    stats_fit = tools.Statistics(lambda ind: ind.fitness.values)
-    stats_size = tools.Statistics(len)
-    mstats = tools.MultiStatistics(fitness=stats_fit, size=stats_size)
-    mstats.register("avg", np.mean)
-    mstats.register("std", np.std)
-    mstats.register("min", np.min)
-    mstats.register("max", np.max)
-
-    # pop, log = algorithms.eaSimple(pop, toolbox, 0.5, 0.5, 100, stats=mstats,
-    #                                halloffame=hof, verbose=True)
-
-    pop, log = algorithms.eaMuPlusLambda(pop, toolbox, 100, pop_num, 0.5, 0.5, 100, stats=mstats,
-                                   halloffame=hof, verbose=True)
-
-    # pop, log = algorithms.eaMuCommaLambda(pop, toolbox, 100, 300, 0.5, 0.5, 100, stats=mstats,
-    #                                       halloffame=hof, verbose=True)
-
-    validation_fitness = fitness_function(test_data, hof.items[0])[0]
-
-    external_call = True
-    pred_effort, actual_effort = fitness_function(test_data, hof.items[0])
-    external_call = False
-
-    print()
-    print("Training Output:")
-    print("Training Data Rows:", len(train_data))
-    print("Fittest Individual (HoF):", hof.items[0])
-    print("Fitness of Best Model (MMRE):", str(hof.items[0].fitness)[1:-2])
-    print()
-    print("Model Validation with Test Data:")
-    print("Test Data Rows:", len(test_data))
-    print("Test Data Fitness (MMRE):", validation_fitness)
-    print()
-
-    return hof, validation_fitness, pred_effort, actual_effort
 
 
 def exec_train_test_split(df, pset, is_ea):
@@ -108,7 +45,7 @@ def exec_train_test_split(df, pset, is_ea):
         actual_effort_train = np.expm1(train_info[1])
         pred_effort_test = np.expm1(test_info[0])
         actual_effort_test = np.expm1(test_info[1])
-        plot_data(pred_effort_train, actual_effort_train, pred_effort_test, actual_effort_test)
+        # plot_data(pred_effort_train, actual_effort_train, pred_effort_test, actual_effort_test)
 
 
 def exec_k_fold(df, pset, is_ea):
@@ -201,17 +138,99 @@ def exec_k_fold(df, pset, is_ea):
         # lr_pred_effort_test = np.expm1(lr_pred_effort_test)
         # lr_actual_effort_test = np.expm1(lr_actual_effort_test)
 
-        plot_data(lr_pred_effort_train, test_x, lr_pred_effort_test, lr_actual_effort_test)
+        # plot_data(lr_pred_effort_train, test_x, lr_pred_effort_test, lr_actual_effort_test)
 
 
-def pred(pred, actual, n):
-    less_than_n = 0
-    for i in range(len(pred)):
-        distance_pc = (abs(actual[i] - pred[i]) / actual[i]) * 100
-        if distance_pc <= n:
-            less_than_n += 1
-    value = (less_than_n / len(pred)) * 100
-    return value
+def execute_ea(pset, train_data, test_data):
+    global external_call
+    pop_num = 300
+    external_call = False
+    creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+    creator.create("Individual", gp.PrimitiveTree, fitness=creator.FitnessMin)
+
+    toolbox = base.Toolbox()
+    toolbox.register("expr", gp.genHalfAndHalf, pset=pset, min_=1, max_=2)
+    toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.expr)
+    toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+    toolbox.register("compile", gp.compile, pset=pset)
+    fitness_function = get_fitness(toolbox, 3)
+    toolbox.register("evaluate", fitness_function, train_data)
+    toolbox.register("select", tools.selTournament, tournsize=3)
+    toolbox.register("mate", gp.cxOnePoint)
+    toolbox.register("expr_mut", gp.genFull, min_=0, max_=2)
+    toolbox.register("mutate", gp.mutUniform, expr=toolbox.expr_mut, pset=pset)
+
+    toolbox.decorate("mate", gp.staticLimit(key=operator.attrgetter("height"), max_value=17))
+    toolbox.decorate("mutate", gp.staticLimit(key=operator.attrgetter("height"), max_value=17))
+
+    random.seed(1000)
+    pop = toolbox.population(n=pop_num)
+    hof = tools.HallOfFame(1)
+
+    stats_fit = tools.Statistics(lambda ind: ind.fitness.values)
+    stats_size = tools.Statistics(len)
+    mstats = tools.MultiStatistics(fitness=stats_fit, size=stats_size)
+    mstats.register("avg", np.mean)
+    mstats.register("std", np.std)
+    mstats.register("min", np.min)
+    mstats.register("max", np.max)
+
+    # pop, log = algorithms.eaSimple(pop, toolbox, 0.5, 0.5, 100, stats=mstats,
+    #                                halloffame=hof, verbose=True)
+
+    pop, log = algorithms.eaMuPlusLambda(pop, toolbox, 100, pop_num, 0.5, 0.5, 100, stats=mstats,
+                                   halloffame=hof, verbose=True)
+
+    # pop, log = algorithms.eaMuCommaLambda(pop, toolbox, 100, 300, 0.5, 0.5, 100, stats=mstats,
+    #                                       halloffame=hof, verbose=True)
+
+    validation_fitness = fitness_function(test_data, hof.items[0])[0]
+
+    external_call = True
+    pred_effort, actual_effort = fitness_function(test_data, hof.items[0])
+    external_call = False
+
+    print()
+    print("Training Output:")
+    print("Training Data Rows:", len(train_data))
+    print("Fittest Individual (HoF):", hof.items[0])
+    print("Fitness of Best Model (MMRE):", str(hof.items[0].fitness)[1:-2])
+    print()
+    print("Model Validation with Test Data:")
+    print("Test Data Rows:", len(test_data))
+    print("Test Data Fitness (MMRE):", validation_fitness)
+    print("PRED(25) of Validation Data:", pred(pred_effort, actual_effort, 25), "%")
+    print()
+
+    return hof, validation_fitness, pred_effort, actual_effort
+
+
+def get_fitness(toolbox, dataset_num):
+    def evaluate(df, individual):
+        func = toolbox.compile(expr=individual)
+        magnitude_of_relative_error_list = []
+        pred_effort_list = []
+        actual_effort_list = []
+        for row in df.itertuples():
+            if dataset_num == 1:
+                pred_effort = func(row[1], row[2], row[3], row[4], row[5], row[6])
+                actual_effort = row[7]
+            elif dataset_num == 2:
+                pred_effort = func(row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12], row[13], row[14], row[15])
+                actual_effort = row[16]
+            elif dataset_num == 3:
+                pred_effort = func(row[1], row[2], row[3], row[4], row[5], row[6])
+                actual_effort = row[7]
+            magnitude_of_relative_error = (abs(actual_effort - pred_effort) / actual_effort) * 100
+            magnitude_of_relative_error_list.append(magnitude_of_relative_error)
+            pred_effort_list.append(pred_effort)
+            actual_effort_list.append(actual_effort)
+        global external_call
+        if external_call:
+            return pred_effort_list, actual_effort_list
+        mean_mre = sum(magnitude_of_relative_error_list) / len(df)
+        return mean_mre,
+    return evaluate
 
 
 def execute_lr(train_data, test_data):
@@ -251,6 +270,7 @@ def execute_lr(train_data, test_data):
     print("Model Validation with Test Data:")
     print("Test Data Rows:", len(test_data))
     print("Model Validation Fitness (MMRE):", mmre_test)
+    print("PRED(25) of Validation Data:", pred(pred_effort_test, actual_effort_test, 25), "%")
     print()
 
     train_info = pred_effort_train, actual_effort_train, mmre_train
@@ -258,11 +278,45 @@ def execute_lr(train_data, test_data):
     return train_info, test_info
 
 
+def pred(pred, actual, n):
+    less_than_n = 0
+    for i in range(len(pred)):
+        distance_pc = (abs(actual[i] - pred[i]) / actual[i]) * 100
+        if distance_pc <= n:
+            less_than_n += 1
+    value = (less_than_n / len(pred)) * 100
+    return value
+
+
+def loop_thru_zip(x, y):
+    mres = []
+    for pred_effort, actual_effort in zip(x, y):
+        mre = (abs(actual_effort - pred_effort) / actual_effort) * 100
+        mres.append(mre)
+    mmre = sum(mres) / len(x)
+    return mmre
+
+
 def plot_data(pred_effort_train, test_x, lr_pred_effort_test, lr_actual_effort_test):
     global j
     global pwd
     global gp_pred_effort
     global gp_actual_effort
+
+    np.save("gp_pred_effort", gp_pred_effort)
+    np.save("gp_actual_effort", gp_actual_effort)
+    np.save("lr_pred_effort_test", lr_pred_effort_test)
+    np.save("lr_actual_effort_test", lr_actual_effort_test)
+
+    # gp_pred_effort = np.load("gp_pred_effort.npy")
+    # gp_actual_effort = np.load("gp_actual_effort.npy")
+    # lr_pred_effort_test = np.load("lr_pred_effort_test.npy")
+    # lr_actual_effort_test = np.load("lr_actual_effort_test.npy")
+
+    # gp_pred_effort = np.log1p(gp_pred_effort)
+    # gp_actual_effort = np.log1p(gp_actual_effort)
+    # lr_pred_effort_test = np.log1p(lr_pred_effort_test)
+    # lr_actual_effort_test = np.log1p(lr_actual_effort_test)
 
     mean_log = np.mean(gp_actual_effort)
     mean_arr_log = np.zeros(len(gp_actual_effort))
@@ -277,23 +331,23 @@ def plot_data(pred_effort_train, test_x, lr_pred_effort_test, lr_actual_effort_t
     plt.scatter(lr_pred_effort_test, lr_actual_effort_test, c="lightgreen", marker="D", edgecolors='black', label="Linear Regression")
     plt.scatter(mean_arr_log, gp_actual_effort, 4, marker='D', c="blue", label="Mean Effort")
     # plt.scatter(median_arr, gp_actual_effort, 4, marker='D', c="#ff00ff", label="Median Effort")
-    plt.plot([0, 4], [0, 4], c="black", label="Ideal Model")
+    # plt.plot([7, 10], [7, 10], c="black", label="Ideal Model")
     plt.title("Effort Predictions - Validation Data - China")
     plt.xlabel("Predicted Effort (log(1 + x))")
     plt.ylabel("Actual Effort (log(1 + x))")
-    # plt.xlim((0.8, 4))
-    # plt.ylim((0, 4))
+    # plt.xlim((7, 10))
+    # plt.ylim((6, 11))
     plt.legend(loc="upper left")
     # plt.plot(test_x, lr_pred_effort_test, c="#ffa500", label="Ideal Model")
     # plt.plot([mean, mean],[0,100], c='blue', label="Mean Effort")
     # plt.plot([median, median], [0, 100], c='#ff00ff', label="Median Effort")
-    graph_path = os.path.join(pwd, '{}'.format(str(j) + 'newpred_comparison.pdf'))
+    graph_path = os.path.join(pwd, '{}'.format(str(j) + 'test_scatter.pdf'))
     fig.savefig(graph_path, bbox_inches="tight")
 
-    gp_pred_effort = np.expm1(gp_pred_effort)
-    gp_actual_effort = np.expm1(gp_actual_effort)
-    lr_pred_effort_test = np.expm1(lr_pred_effort_test)
-    lr_actual_effort_test = np.expm1(lr_actual_effort_test)
+    # gp_pred_effort = np.expm1(gp_pred_effort)
+    # gp_actual_effort = np.expm1(gp_actual_effort)
+    # lr_pred_effort_test = np.expm1(lr_pred_effort_test)
+    # lr_actual_effort_test = np.expm1(lr_actual_effort_test)
 
     mean = np.mean(gp_actual_effort)
     mean_arr = np.zeros(len(gp_actual_effort))
@@ -331,7 +385,7 @@ def plot_data(pred_effort_train, test_x, lr_pred_effort_test, lr_actual_effort_t
     bplot.set_xlabel("Search Technique")
     bplot.set_ylabel("z value")
     # plt.ylim((-0.5, 8))
-    graph_path = os.path.join(pwd, '{}'.format(str(j) + 'boxplot.pdf'))
+    graph_path = os.path.join(pwd, '{}'.format(str(j) + 'test_boxplot.pdf'))
     fig.savefig(graph_path, bbox_inches="tight")
 
     # # Train Test Split Plot predictions - Training vs Validation Data
@@ -358,43 +412,6 @@ def plot_data(pred_effort_train, test_x, lr_pred_effort_test, lr_actual_effort_t
     # plt.hlines(y=0, xmin=10.5, xmax=13.5, color="red")
     # graph_path = os.path.join(pwd, '{}'.format(str(j) + 'resid.pdf'))
     # fig.savefig(graph_path, bbox_inches="tight")
-
-
-def loop_thru_zip(x, y):
-    mres = []
-    for pred_effort, actual_effort in zip(x, y):
-        mre = (abs(actual_effort - pred_effort) / actual_effort) * 100
-        mres.append(mre)
-    mmre = sum(mres) / len(x)
-    return mmre
-
-
-def get_fitness(toolbox, dataset_num):
-    def evaluate(df, individual):
-        func = toolbox.compile(expr=individual)
-        magnitude_of_relative_error_list = []
-        pred_effort_list = []
-        actual_effort_list = []
-        for row in df.itertuples():
-            if dataset_num == 1:
-                pred_effort = func(row[1], row[2], row[3], row[4], row[5], row[6])
-                actual_effort = row[7]
-            elif dataset_num == 2:
-                pred_effort = func(row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12], row[13], row[14], row[15])
-                actual_effort = row[16]
-            elif dataset_num == 3:
-                pred_effort = func(row[1], row[2], row[3], row[4], row[5], row[6], row[7])
-                actual_effort = row[8]
-            magnitude_of_relative_error = (abs(actual_effort - pred_effort) / actual_effort) * 100
-            magnitude_of_relative_error_list.append(magnitude_of_relative_error)
-            pred_effort_list.append(pred_effort)
-            actual_effort_list.append(actual_effort)
-        global external_call
-        if external_call:
-            return pred_effort_list, actual_effort_list
-        mean_mre = sum(magnitude_of_relative_error_list) / len(df)
-        return mean_mre,
-    return evaluate
 
 
 def prep_albrecht(df):
@@ -448,6 +465,7 @@ def prep_china(df):
 
 def prep_desharnais(df):
     df = df.drop(columns='Project')
+    df = df.drop(columns='YearEnd')
     df = df.drop(columns='Adjustment')
     df = df.drop(columns='PointsAjust')
     df = df.drop(columns='Language')
@@ -455,7 +473,7 @@ def prep_desharnais(df):
     df = df.drop(labels=['Effort'], axis=1)
     df.insert(len(df.columns), 'Effort', effort_col)
     df.Effort = np.log1p(df.Effort)
-    pset = gp.PrimitiveSet("main", 7)       # Num of inputs (cols)
+    pset = gp.PrimitiveSet("main", 6)       # Num of inputs (cols)
     pset.addPrimitive(operator.add, 2)      # Num of arguments (a+b)
     pset.addPrimitive(operator.sub, 2)
     pset.addPrimitive(operator.mul, 2)
@@ -464,11 +482,10 @@ def prep_desharnais(df):
     pset.addEphemeralConstant("rand101", lambda: random.randint(0, 100))
     pset.renameArguments(ARG0="TeamExp")
     pset.renameArguments(ARG1="ManagerExp")
-    pset.renameArguments(ARG2="YearEnd")
-    pset.renameArguments(ARG3="Length")
-    pset.renameArguments(ARG4="Transactions")
-    pset.renameArguments(ARG5="Entities")
-    pset.renameArguments(ARG6="PointsNonAdjust")
+    pset.renameArguments(ARG2="Length")
+    pset.renameArguments(ARG3="Transactions")
+    pset.renameArguments(ARG4="Entities")
+    pset.renameArguments(ARG5="PointsNonAdjust")
     return df, pset
 
 
